26d99b5047299e196418d637c049bc8e060a159c
darwinrobalino12 <dv.robalinob@uea.edu.ec>
2024-12-09 15:43:05 -0500

    deber semana 3

---
+++ deber semana 3/solucion programacion orientada objetos.py
@@ -0,0 +1,25 @@
+# Programación Orientada a Objetos (POO)
+# iniciamos con una lista vacia para almacenar las temperaturas
+class Clima:
+
+# constructor de la clase clima
+    def __init__(self):
+        self.temperaturas = []
+#metodo para ingresar el valor de la temperatura diaria
+    def ingresar_temperatura(self, temperatura):
+                self.temperaturas.append(temperatura)
+                #metodo para calcular el promedio
+    def calcular_promedio(self):
+        return sum(self.temperaturas) / len(self.temperaturas)
+#funcion que ejecuta el programa
+def main():
+    clima = Clima()
+    print("Ingrese el valor de las temperaturas diarias durante una semana:")
+    for dia in range(1, 8):
+        temperatura = float(input(f"Ingrese el valor de  la temperatura del día {dia}: "))
+        clima.ingresar_temperatura(temperatura)
+    promedio = clima.calcular_promedio()
+    print(f"El promedio semanal de temperaturas es: {promedio:.2f} grados.")
+
+if __name__ == "__main__":
+    main()
