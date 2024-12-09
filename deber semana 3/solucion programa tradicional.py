5810bc608cf900b1f77057eb799ae70faaf37463
darwinrobalino12 <dv.robalinob@uea.edu.ec>
2024-12-09 15:43:05 -0500

    deber semana 3

---
+++ deber semana 3/solucion programa tradicional.py
@@ -0,0 +1,26 @@
+# Programación Tradicional
+# Función para ingresar las temperaturas diarias durante la semana
+def ingresar_temperaturas():
+    """Función para ingresar las temperaturas de cada día de la semana."""
+    temperaturas = []
+    for dia in range(1, 8):
+        temperatura = float(input(f"Ingrese la temperatura del día tomada al mismo horario {dia}: "))
+        temperaturas.append(temperatura)
+    return temperaturas
+
+# Función para calcular el promedio de la semana
+def calcular_promedio(temperaturas):
+    """Función para calcular el promedio de las temperaturas semanales."""
+    return sum(temperaturas) / len(temperaturas)
+
+# Función que ejecuta el programa
+def main():
+    """Función principal del programa."""
+    print("Ingrese las temperaturas diarias durante una semana:")
+    temperaturas = ingresar_temperaturas()
+    promedio = calcular_promedio(temperaturas)
+    print(f"El promedio semanal de temperaturas es: {promedio:.2f} grados.")
+
+if __name__ == "__main__":
+    main()
+
