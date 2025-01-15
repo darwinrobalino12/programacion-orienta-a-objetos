2a1b576012a74be9dcac71c32d30365e67ce28b9
darwinrobalino12 <dv.robalinob@uea.edu.ec>
2025-01-14 21:09:07 -0500

    tarea semana 6

---
+++ deber semana 6/definicion de clase,obj,herencia.py
@@ -0,0 +1,27 @@
+# Definición de la clase base
+class Vehiculo:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def describir(self):
+        return f"Vehículo marca {self.marca}, modelo {self.modelo}"
+
+# Definición de la clase derivada
+class Motocicleta(Vehiculo):
+    def __init__(self, marca, modelo, velocidad_maxima):
+        # Llamamos al constructor de la clase base
+        super().__init__(marca, modelo)
+        self.velocidad_maxima = velocidad_maxima  # Atributo público
+
+    # Método sobrescrito Polimorfismo
+    def describir(self):
+        return f"Motocicleta marca {self.marca}, modelo {self.modelo}, velocidad máxima {self.velocidad_maxima} km/h"
+
+# Instanciación de objetos
+vehiculo = Vehiculo("Toyota", "4runner")
+moto = Motocicleta("Honda", "crf230", 125)
+
+# Mostramos las descripciones
+print(vehiculo.describir())  # Salida: Vehículo marca Toyota, modelo Corolla
+print(moto.describir())      # Salida: Motocicleta marca Harley Davidson, modelo Sportster, velocidad máxima 200 km/h
